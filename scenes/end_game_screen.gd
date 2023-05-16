4a7b81a703a144adc590b34363b9fbe501ada53d
ash4rk <cool.shark.98@mail.ru>
2023-05-16 16:55:56 +0300

    FEAT: Add percentage to end game screen with color ratio

--- scenes/end_game_screen.gd
+++ scenes/end_game_screen.gd
@@ -5,10 +5,15 @@ const MAIN_MENU_SCREEN_PATH = "res://main.tscn"
 var correct_answers: int
 var number_of_questions: int
 @onready var correct_answers_value_label = $VBoxContainer/CorrectAnswersValueLabel
+@onready var percentage_label = $VBoxContainer/PercentageLabel
 @onready var new_game_button = $NewGameButton
+
 
 func _ready():
 	correct_answers_value_label.text = str(correct_answers) + "/" + str(number_of_questions)
+	var percent = float(correct_answers)/float(number_of_questions)
+	percentage_label.text = str(snapped(percent*100, 0.01)) + "%"
+	percentage_label.modulate = Color.DARK_RED.lerp(Color.GREEN, percent)
 
 func _process(delta):
 	new_game_button.disabled = SceneManager.is_transitioning
