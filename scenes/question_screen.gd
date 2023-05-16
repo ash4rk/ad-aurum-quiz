97fab082fe1942e07febfa7fd1a20a286b3d814d
ash4rk <cool.shark.98@mail.ru>
2023-05-16 10:14:41 +0300

    FIX(WIP): Develop question_screen

--- scenes/question_screen.gd
+++ scenes/question_screen.gd
@@ -4,6 +4,7 @@ const ANSWER_BUTTON_SCENE = preload("res://scenes/answer_button.tscn")
 const QUESTIONS_JSON_PATH = "res://questions.json"
 
 var question: Dictionary
+var question_idx: int = 0
 #question.question - question text
 #question.background - image path (like "backgrounds/osmium.jpg")
 #question.answers - array of answers
@@ -11,11 +12,13 @@ var question: Dictionary
 @onready var question_label = $CanvasLayer/PanelContainer/QuestionLabel
 @onready var bg = $CanvasLayer/BG
 @onready var answer_buttons_container = $CanvasLayer/PanelContainer2/AnswerButtonsContainer
+@onready var popup = $CanvasLayer/FalseCorrectPopup
+@onready var popup_label = $CanvasLayer/FalseCorrectPopup/PanelContainer/VBoxContainer/FalseCorrectLabel
 
 func _ready():
 	var json_string = FileAccess.get_file_as_string(QUESTIONS_JSON_PATH)
 	var questions_data: Array = JSON.parse_string(json_string)
-	question = questions_data[0]
+	question = questions_data[question_idx]
 	
 	bg.texture = load("res://" + question.background)
 	question_label.text = question.question
@@ -31,6 +34,16 @@ func _init_answer_buttons(answers: Array) -> void:
 
 func _check_correctness(is_correct: bool) -> void:
 	if is_correct:
-		print("Correct answer!")
+		popup_label.text = "Correct answer!"
 	else:
-		print("Wrong answer!")
+		popup_label.text = "Wrong answer!"
+	popup.visible = true
+
+func _on_next_question_button_pressed():
+	popup.visible = false
+	question_idx += 1
+	for n in answer_buttons_container.get_children():
+		answer_buttons_container.remove_child(n)
+		n.queue_free()
+	_ready()
+	
