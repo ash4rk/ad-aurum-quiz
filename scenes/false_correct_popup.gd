f018c69cf494e602bdd6fb34ebbeca9d5994d2b0
ash4rk <cool.shark.98@mail.ru>
2023-05-16 16:07:05 +0300

    FIX: Add font, theme. Configure visual

--- scenes/false_correct_popup.gd
+++ scenes/false_correct_popup.gd
@@ -2,15 +2,19 @@ extends Control
 
 class_name FalseCorrectPopup
 
+@export_color_no_alpha var correct_modulate
+@export_color_no_alpha var false_modulate
 @onready var false_correct_label = $PanelContainer/VBoxContainer/FalseCorrectLabel
 
 func show_correct():
 	self.visible = true
 	false_correct_label.text = "Верно"
+	self.modulate = correct_modulate
 
 func show_wrong():
 	self.visible = true
 	false_correct_label.text = "Неверно"
+	self.modulate = false_modulate
 
 func _on_next_question_button_pressed():
 	Global.load_next_screen()
